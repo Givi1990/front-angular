35844e1593ab18a985112dce796766fe5e6805ff
Givi <Givi.givi.khaduri@gmail.com>
2024-10-26 20:17:41 +0400

    f18 c

--- src/app/services/baseUrl.ts
+++ src/app/services/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://server-js-s6bw.onrender.com';
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = 'https://server-js-s6bw.onrender.com';
+// export const baseUrl = "http://localhost:5000";
